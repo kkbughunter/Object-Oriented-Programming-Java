a48ed5601864034cd3bec3da804c8a9363445cd1
Karthikeyan_A <91019132+KKBUGHUNTER@users.noreply.github.com>
2022-10-28 12:29:41 +0000

    Update Hierarchy.java

--- Assignment-3/Hierarchy.java
+++ Assignment-3/Hierarchy.java
@@ -1,3 +1,5 @@
+// please check the CGPA calculation 
+
 import java.util.*;
 
 class Person{
@@ -5,12 +7,12 @@ class Person{
 	private String name,address;
 	private char gender;
 	public static Scanner sc = new Scanner(System.in);
-	
-	Person(int aadhaar, String name, String adderss,  char gender){
-		this.aadhaar = aadhaar;
-		this.name = name;
-		this.address = address;
-		this.gender = gender;
+	Person(){}
+	Person(int aa, String n, String add,  char g){
+		aadhaar = aa;
+		name = n;
+		address = add;
+		gender = g;
 	}
 	public String getName(){
 		String n = sc.nextLine();
@@ -29,21 +31,24 @@ class Person{
 		address = addr;
 		System.out.println("\t\tAddress added Successfuly");
 	}
+	public void displayPer(){
+		System.out.println("Name : " + name);
+		System.out.println("Address : " + address);
+		System.out.println("Aadhaar : " + aadhaar);
+		System.out.println("Gender : " + gender);
+	}
 }
-
-
-
-
 class Faculty extends Person{
 	private String designation, department;
 	private float basicpay;
+	double Net_salary;
 	Faculty(int aa,String n,String a,char g,String de,String dept,float pay){
 		super(aa, n, a, g);
 		designation = de;
 		department = dept;
 		basicpay = pay;
 	}
-	
+
 	public String getDesig(){
 		String n = sc.nextLine();
 		return n;
@@ -62,27 +67,37 @@ class Faculty extends Person{
 	}
 	public void calSalary(float pay){
 		double DA = (60/100)*pay , HRA = (10/100)*pay,Medical_Insurance = (8.5/100)*pay,PF = (8/100)*pay ;
-		double Gross_salary = pay + DA + HRA; 			
+		double Gross_salary = pay + DA + HRA;
 		double Deductions = Medical_Insurance  + PF;
-		double Net_salary = Gross_salary - Deductions; 
+		Net_salary = Gross_salary - Deductions;
+	}
+	public void displayFal(){
+		
+		System.out.println("Department : "+department);
+		System.out.println("Designation : "+designation);
+		System.out.println("Basicpay : "+basicpay);
+		System.out.println("Net salary : "+Net_salary);
 	}
 }
 
 
 class Student extends Person{
-	
+
 	private String program;
 	int year, sub1_credit,sub2_credit,sub3_credit;
+	float CGPA;
 	char sub1_grade, sub2_grade, sub3_grade;
+	Student(){}
 	Student(int aa,String n,String a,char g,String p,int y,char s1g,char s2g,char s3g,int s1c,int s2c,int s3c){
 		super(aa, n, a, g);
+		program=p;
 		year=y;
- 		sub1_credit=s1c ;
+		sub1_credit=s1c ;
 		sub2_credit=s2c;
 		sub3_credit=s3c;
 		sub1_grade=s1g;
- 		sub2_grade=s2g;
- 		sub3_grade=s3g;
+		sub2_grade=s2g;
+		sub3_grade=s3g;
 	}
 
 	public String getProgram(){
@@ -93,7 +108,7 @@ class Student extends Person{
 		int n = sc.nextInt();
 		return n;
 	}
-//--------------
+	//--------------
 	public int getsub1_credit(){
 		int n = sc.nextInt();
 		return n;
@@ -106,8 +121,8 @@ class Student extends Person{
 		int n = sc.nextInt();
 		return n;
 	}
-	
-//--------------
+
+	//--------------
 	public char getsub1_grade(){
 		char n = sc.next().charAt(0);
 		return n;
@@ -120,25 +135,112 @@ class Student extends Person{
 		char n = sc.next().charAt(0);
 		return n;
 	}
-//--------------------------------------------------------------
+	//--------------------------------------------------------------
 
-	
-
-
-
-} 
-
-
-
-
+	private void setsub1_grade(char g){
+		sub1_grade = g;
+		System.out.println("\tUpdated added Successfuly");
+	}
+	private void setsub2_grade(char g){
+		sub2_grade = g;
+		System.out.println("\tUpdated added Successfuly");
+	}
+	private void setsub3_grade(char g){
+		sub3_grade = g;
+		System.out.println("\tUpdated added Successfuly");
+	}
+	private void setsub1_credit(int c){
+		sub1_credit = c;
+		System.out.println("\tUpdated added Successfuly");
+	}
+	private void setsub2_credit(int c){
+		sub2_credit = c;
+		System.out.println("\tUpdated added Successfuly");
+	}
+	private void setsub3_credit(int c){
+		sub3_credit = c;
+		System.out.println("\tUpdated added Successfuly");
+	}
+	public void calGPA(){
+		int gr1 = (int) sub1_grade;
+		gr1 = (gr1 - 70)*-1;
+		int gr2 = (int) sub2_grade;
+		gr2 = (gr2 - 70)*-1;
+		int gr3 = (int) sub3_grade;
+		gr3 = (gr3 - 70)*-1;
+		CGPA = ((gr1*sub1_credit)+(gr1*sub1_credit)+(gr1*sub1_credit))/(sub1_credit+sub2_credit+sub3_credit);
+	}
+	public void displayStu(){
+		System.out.println("Program : "+program);
+		System.out.println("Year : "+year);
+		System.out.println("CGPA : "+CGPA);
+	}
+}
 
 class Hierarchy{
 	public static void main(String[] argc){
-	 	
-		Person p = new Person(1,"2","3",'4');
-
-		p.setAddress(p.getAddress());
-		
+		Scanner sc = new Scanner(System.in);
+		int aa,year,c1,c2,c3,opt;
+		float pay;
+		String dept,de,name,add,prog;
+		char g,g1,g2,g3;
+		System.out.print("1. Student \n2. Faculity\nEnter your option : ");
+		opt = sc.nextInt();
+		switch(opt){
+			case 1:
+				System.out.print("Enter your Name : ");
+				name = sc.nextLine();
+				name = sc.nextLine();
+				System.out.print("Enter your address : ");
+				add = sc.nextLine();
+				System.out.print("Enter your Gender : ");
+				g = sc.next().charAt(0);
+				System.out.print("Enter your Program : ");
+				prog = sc.nextLine();
+				prog = sc.nextLine();
+				System.out.print("Enter your Aadhaar No : ");
+				aa = sc.nextInt();
+				System.out.print("Enter your year : ");
+				year = sc.nextInt();
+				System.out.print("Enter your grade 1 : ");
+				g1 = sc.next().charAt(0);
+				System.out.print("Enter your grade 2 : ");
+				g2 = sc.next().charAt(0);
+				System.out.print("Enter your grade 3 : ");
+				g3 = sc.next().charAt(0);
+				System.out.print("Enter your credit 1 : ");
+				c1 = sc.nextInt();
+				System.out.print("Enter your credit 2 : ");
+				c2 = sc.nextInt();
+				System.out.print("Enter your credit 3 : ");
+				c3 = sc.nextInt();
+				Student s = new Student(aa,name,add,g,prog,year,g1,g2,g3,c1,c2,c3);
+				s.displayPer();
+				s.displayStu();
+				break;
+			case 2:
+				System.out.print("Enter your Name : ");
+				name = sc.nextLine();
+				name = sc.nextLine();
+				System.out.print("Enter your address : ");
+				add = sc.nextLine();
+				System.out.print("Enter your Gender : ");
+				g = sc.next().charAt(0);
+				System.out.print("Enter your Aadhaar No : ");
+				aa = sc.nextInt();
+				System.out.println("Enter the Department : ");
+				dept = sc.nextLine();
+				dept = sc.nextLine();
+				System.out.println("Enter the Designation : ");
+				de = sc.nextLine();
+				System.out.println("Enter the Basicpay : ");
+				pay = sc.nextFloat();
+				Faculty f = new Faculty(aa, name, add, g, de, dept, pay);
+				f.calSalary(pay);
+				f.displayPer();
+				f.displayFal();
+			default:System.out.println("Enter the correct output...");
+		}
 	}
 
 }
