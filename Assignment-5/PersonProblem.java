0e7d76e4740b101a65b179a3302b9350d45c985e
Karthikeyan_A <91019132+KKBUGHUNTER@users.noreply.github.com>
2022-11-23 21:39:16 +0530

    Add files via upload

---
+++ Assignment-5/PersonProblem.java
@@ -0,0 +1,100 @@
+import java.util.Scanner;
+
+class MyException extends Exception {
+    public MyException(String s)
+    {
+        super(s);
+    }
+}
+class Person {
+    Scanner sc = new Scanner(System.in);        
+    private String name, aadharnumber=null;
+    private int age;
+    public void getInput(){
+        System.out.print("Enter your Name : ");
+        name = sc.nextLine();
+        int flag =2;
+        while(flag != 0){
+            try{
+                System.out.print("Enter your Age : ");
+                String age1 = sc.nextLine();
+                age = Integer.parseInt(age1);
+                flag = 2;
+            }
+            catch(NumberFormatException e){
+                System.out.println("Please enter the correct age...");
+                flag = 1;
+            }
+            if(flag != 1) flag = 0;
+        }
+        System.out.print("Enter your Aadhar Number : ");
+        aadharnumber = sc.nextLine();
+    }
+
+    public void canVote(){
+        int flag =0;
+        try{
+            if(age <=18) throw new MyException("MinorCitizenException");
+        }catch (MyException e) {
+            System.out.println("\t\t\t" + e.getMessage());
+            flag =1;
+        }
+        if(flag == 0) System.out.println("\t\t\tYou can Vote");
+    }
+
+    public void hasAadhar(){
+        try{
+            
+            if(aadharnumber.equals("")) throw new NullPointerException();
+            System.out.println("\t\t\tAadher Number is Valid");
+            
+        }
+        catch(NullPointerException e){
+            System.out.println("\t\t\tNullPonterException");
+        }
+    }
+    public void display(){
+        System.out.println("D\t\tData");
+        System.out.println("Name ; " + name);
+        System.out.println("Age : " + age);
+        System.out.println("Aadher no : " + aadharnumber);
+        System.out.println("....Thank You....");
+    }
+    
+}
+
+
+class PersonProblem{
+    public static void main(String[] args) {
+        Person p = new Person();
+        Scanner sc = new Scanner(System.in); 
+        String clear;  
+        while(true){
+            System.out.print("\t\t Menu Bar\n1. Get Input\n2.Can Vote\n3. Check Aadher\n4. Display\nEnter your option : ");
+            int opt = sc.nextInt();
+            switch(opt){
+                case 1:
+                    p.getInput();
+                    break;
+                case 2:
+                    p.canVote();
+                    break;
+                case 3:
+                    p.hasAadhar();
+                    break;
+                case 4:
+                    p.display();
+                    break;
+                    
+            }
+
+            System.out.print("press any key to continue...");
+            clear = sc.nextLine();
+            clear = sc.nextLine();
+            System.out.print("\033[H\033[2J");  
+            System.out.flush();  
+            
+        }
+        
+    }
+}
