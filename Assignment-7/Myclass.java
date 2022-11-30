9f8389af39f0b2ce608b073454b748b1c31667f5
Karthikeyan_A <91019132+KKBUGHUNTER@users.noreply.github.com>
2022-11-30 13:21:55 +0530

    Add files via upload

---
+++ Assignment-7/Myclass.java
@@ -0,0 +1,18 @@
+class Maximum{
+	<T extends Comparable> T findMax(T a, T b){
+		if(a.compareTo(b) > 0)
+			return a;
+		return b;
+	}
+
+}
+
+public class Myclass{
+	public static void main(String[] args){
+		Maximum max = new Maximum();
+		System.out.println(max.findMax(2342,234));
+		System.out.println(max.findMax(2216.7,713.34));
+		System.out.println(max.findMax("SSN","IIT Madras"));
+	}
+
+}
