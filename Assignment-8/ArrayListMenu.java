6af53eb3662888ae3420e8a07e82a50c3c3fe24e
Karthikeyan_A <91019132+KKBUGHUNTER@users.noreply.github.com>
2022-12-16 21:09:15 +0530

    Add files via upload

--- Assignment-8/ArrayListMenu.java
+++ Assignment-8/ArrayListMenu.java
@@ -3,77 +3,52 @@ class ArrayListMenu {
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        LinkedList<String> arr = new LinkedList<String>();
+        LinkedList<Integer> arr1 = new LinkedList<Integer>();
+        LinkedList<Integer> arr2 = new LinkedList<Integer>();
+        arr1.add(10);
+        arr1.add(20);
+        arr1.add(30);
+        arr1.add(40);
+        arr2.add(30);
+        arr2.add(40);
+        arr2.add(50);
+        arr2.add(60);
 
-
-        System.out.println("1. Append - add at end\n2. Insert  add at particular index\n3. Find the index of a particular element (Search)\n4. Display the list\n5. List all string starts with given letter\n6. List of all string contains the Substring\n7. Sort the elements in ArrayList\n8. Remove a particular element\n9. Replace one string with another string in ArrayList\n10. Remove duplicate elements");
-
+        System.out.println("1. Merge the two lists\n2. Find Union of two lists\n3. Find Intersection of two lists\n4. Compare two lists\n");
+        System.out.println("List 1 :" + arr1);
+        System.out.println("List 2 :" + arr2);
         while(true){
             System.out.print("enter your option : ");
             int opt = sc.nextInt();
             switch(opt){
                 case 1:
-                    System.out.print("enter the element to be inserted : ");
-                    String ele = sc.next();
-                    arr.add(ele);
-                    System.out.println(arr);
+                    arr1.addAll(arr2);
+                    System.out.println("Updated List : " + arr1);
                     break;
                 case 2:
-                    System.out.print("enter the element to be inserted : ");
-                    ele = sc.next();
-                    System.out.print("enter the element Index : ");
-                    int index = sc.nextInt();
-                    index--;
-                    arr.add(index, ele);
-                    System.out.println(arr);
+                    for(Integer i: arr2)
+                            if(arr1.indexOf(i) == -1)
+                                arr1.add(i);
+                    System.out.println("Updated List : " + arr1);
                     break;
                 case 3:
-                    System.out.print("enter the element to be inserted find Index : ");
-                    ele = sc.next();
-                    System.out.println("Index of a element is : " + arr.indexOf(ele));
+                    int flag =0;
+                    for(Integer i: arr2){
+                        for(Integer j: arr1){
+                            if(i == j){
+                                System.out.println("Ther Intersection value is : " + i);
+                                flag =1;
+                                break;
+                            }
+                        }
+                        if(flag==1)break;
+                    }
                     break;
                 case 4 :
-                    System.out.println("Array elements : " + arr);
-                    break;
-                case 5 :
-                    System.out.print("enter the starting Letter : ");
-                    ele = sc.next();
-                    for(String s : arr)
-                        if(s.startsWith(ele)) System.out.println(s);
-                    break;
-                case 6:
-                    System.out.print("enter the starting Letter : ");
-                    ele = sc.next();
-                    for(String s : arr)
-                        if(s.contains(ele)) System.out.println(s);
-                    break;
-                case 7:
-                    System.out.print("Sorted Array list : ");
-                    Collections.sort(arr);
-                    System.out.print(arr);
-                    break;
-                case 8:
-                    System.out.print("enter the remove element : ");
-                    ele = sc.next();
-                    arr.remove(ele);
-                    System.out.print(arr);
-                    break;
-                case 9:
-                    System.out.print("enter the element to be removed : ");
-                    ele = sc.next();
-                    System.out.print("enter the element to be insert : ");
-                    String el = sc.next();
-                    index = arr.indexOf(ele);
-                    arr.remove(index);
-                    arr.add(index,el);
-                    System.out.println(arr);
-                    break;
-                case 10:
-                    Set<String> set = new LinkedHashSet<String>();
-                    set.addAll(arr);
-                    arr.clear();
-                    arr.addAll(set);
-                    System.out.println(arr);
+                    if(arr1.equals(arr2))
+                        System.out.println("The given list are equal..");
+                    else 
+                        System.out.println("The given list are Not equal..");
                     break;
             }
         }
