345b2d706d82758cd39058fb2c1e350abf49e3dc
Karthikeyan_A <91019132+KKBUGHUNTER@users.noreply.github.com>
2023-01-23 20:13:54 +0530

    Add files via upload

---
+++ Mini Project/Java Mini Project GUI/Account_Assistant/src/main/java/com/mycompany/account_assistant/Account_Assistant.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.account_assistant;
+
+/**
+ *
+ * @author karth
+ */
+public class Account_Assistant {
+
+    public static void main(String[] args) {
+        Register r = new Register();
+        r.setVisible(true);
+    }
+}
